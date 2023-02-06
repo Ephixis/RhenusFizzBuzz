6b668e604dd3d649b586775f172e738ea6189d31
Ephixis <L.Brinkmoeller@gmx.de>
2023-02-06 21:17:11 +0100

    Initial Commit

---
+++ src/test/java/com/rhenus/rhenusfizzbuzz/RhenusFizzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhenus.rhenusfizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhenusFizzBuzzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
