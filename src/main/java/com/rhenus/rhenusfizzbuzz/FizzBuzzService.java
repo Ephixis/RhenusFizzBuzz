104677ea1fa3b0cecf409437088c75cdf6e9993b
Ephixis <L.Brinkmoeller@gmx.de>
2023-02-07 17:11:58 +0100

    FizzBuzz is now displayed in JSON format.

---
+++ src/main/java/com/rhenus/rhenusfizzbuzz/FizzBuzzService.java
@@ -0,0 +1,34 @@
+package com.rhenus.rhenusfizzbuzz;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FizzBuzzService {
+
+    public List getFizzBuzz(int length){
+
+        List<String> results = new ArrayList<>();
+
+        if(length<1)
+            return List.of("Number must be >= 1!");
+
+        for(int a=1; a<length; a++){
+
+            if(a%15 == 0){
+                results.add("Fizzbuzz");
+                continue;
+            } else if (a%5==0) {
+                results.add("Buzz");
+                continue;
+            } else if (a%3 == 0) {
+                results.add("Fizz");
+                continue;
+            }
+            results.add(""+a);
+        }
+        return results;
+    }
+}
--- src/main/java/com/rhenus/rhenusfizzbuzz/StartController.java
+++ src/main/java/com/rhenus/rhenusfizzbuzz/StartController.java
@@ -1,20 +1,30 @@
 package com.rhenus.rhenusfizzbuzz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+import java.util.List;
 
 @RestController
 public class StartController {
 
-    @RequestMapping("/")
-    public String helloWorld(){
-        FizzBuzz temp = new FizzBuzz();
-        return temp.getFizzBuzz(36);
+    private final FizzBuzzService fizzBuzzService;
+
+    @Autowired
+    public StartController(FizzBuzzService fizzBuzzService) {
+        this.fizzBuzzService = fizzBuzzService;
     }
 
-    @RequestMapping("/test")
+    @RequestMapping("/FizzBuzz")
+    public List helloWorld(){
+        return fizzBuzzService.getFizzBuzz(36);
+    }
+
+    @RequestMapping("/")
     public String goodbye(){
-        return "test";
+        return "Length";
     }
 
 }
