9b19a8a9d603be0507cdd614bf391b64e9702739
Ephixis <L.Brinkmoeller@gmx.de>
2023-02-08 18:10:55 +0100

    FizzBuzzService.java no longer uses continue.

--- src/main/java/com/rhenus/rhenusfizzbuzz/FizzBuzzService.java
+++ src/main/java/com/rhenus/rhenusfizzbuzz/FizzBuzzService.java
@@ -11,20 +11,17 @@ public class FizzBuzzService {
 
         List<String> results = new ArrayList<>();
 
-        if(length<1)
-            return List.of("Number must be >= 1!");
-
-        for(int a=1; a<length; a++){
+        for(int a=1; a<length+1; a++){
 
             if(a%15 == 0){
                 results.add("Fizzbuzz");
-                continue;
             } else if (a%5==0) {
                 results.add("Buzz");
             } else if (a%3 == 0) {
                 results.add("Fizz");
+            }else {
+                results.add(""+a);
             }
-            results.add(""+a);
         }
         return results;
     }
